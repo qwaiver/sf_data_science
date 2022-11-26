a6f0ad19257bb5ba9f446b739d53dac7a1d12823
Dmitriy <119098491+qwaiver@users.noreply.github.com>
2022-11-26 21:58:23 +0500

    creat project_8, угадывание ~ 12-13 попыток

--- 8. Инструменты Data Saince/project_0/game.py
+++ 8. Инструменты Data Saince/project_0/game.py
@@ -1,23 +1,5 @@
 import numpy as np
 
-def random_predict(number:int=1) -> int:
-    """Рандомно угадываем число
-
-    Args:
-        number (int, optional): Загадонное число. Defaults to 1.
-
-    Returns:
-        int: Число попыток
-    """
-    count = 0
-
-    while True:
-        count += 1
-        predict_number = np.random.randint(1, 101)
-        if number == predict_number:
-            break
-    return count
-
 number = np.random.randint(1,101) # генерируем число
 count = 0 #количество угадываний
 
@@ -30,6 +12,7 @@ while True:
         
     elif predict_number < number:
         print('Число дожно быть больше')
+        
     else:
         print('Поздравляю! Вы угадали число.')
         break
