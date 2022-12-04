db3a0cd561a30c06f15156175f8def4baba6a662
Dmitriy <119098491+qwaiver@users.noreply.github.com>
2022-12-04 22:46:52 +0500

    задания с 6 модуля

---
+++ 9. Библиотка Numpy/типы данных в Numpy.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+np.finfo(np.float16) # границы типа данных с плаваущей запятой
+np.iinfo(np.int16) # границы типа данных целых чисел
+np.iinfo(np.uint16) # границы типа данных целых неотрицательных(беззнаковый тип данных) чисел
+print(*sorted(map(str, set(np.sctypeDict.values()))), sep='\n')
+
+#Всего в выдаче будет 24 строки. Int, uint и float мы уже изучили. 
+# Datetime и timedelta используются для хранения времени, 
+# complex используется для работы с комплéксными числами.
+
+print(np.uint8(-456))
